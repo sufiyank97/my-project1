fa4ddc42e29d878590f446dbafc6a275d6756e18
Sufiyan <sufiyan7697@gmail.com>
2020-11-20 21:16:25 +0530

    'added'

---
+++ config/database.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+      },
+      options: {
+        pool: {
+          min: 0,
+          max: 15,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
+      },
+    },
+  },
+});
